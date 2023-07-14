e184bac603d45eb81b8a5cb8b6c01ab2c90914d2
Poent <Poent@users.noreply.github.com>
2023-07-14 04:29:09 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,186 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
+import relay
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schedules.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    frequency = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.String(255), nullable=True)
+    start_time = db.Column(db.Time, nullable=True)
+    watering_tasks = db.relationship('WateringTask', backref='schedule', lazy=True, cascade='all,delete')
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'frequency': self.frequency,
+            'description': self.description,
+            'start_time': str(self.start_time),
+            'watering_tasks': [watering_task.to_dict() for watering_task in self.watering_tasks]
+        }
+
+
+class Sprinkler(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(255), nullable=False)
+    watering_tasks = db.relationship('WateringTask', backref='sprinkler', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'description': self.description,
+            'watering_tasks': [watering_task.to_dict() for watering_task in self.watering_tasks]
+        }
+
+
+class WateringTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    duration = db.Column(db.Integer, nullable=False)
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'), nullable=False)
+    sprinkler_id = db.Column(db.Integer, db.ForeignKey('sprinkler.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'duration': self.duration,
+            'schedule_id': self.schedule_id,
+            'sprinkler_id': self.sprinkler_id
+        }
+
+
+#===================================================================================================
+# Scheudule routes
+
+@app.route('/schedules', methods=['POST'])
+def create_schedule():
+    data = request.get_json(force=True)
+    schedule = Schedule(name=data.get('name'))
+    db.session.add(schedule)
+    db.session.commit()
+    return jsonify(schedule.to_dict()), 201  # return created schedule
+
+
+
+# GET schedule by id
+@app.route('/schedules/<int:schedule_id>', methods=['GET'])
+def get_schedule(schedule_id):
+    schedule = Schedule.query.get(schedule_id)
+    if schedule is None:
+        return jsonify({'error': 'Schedule not found'}), 404
+    return jsonify(schedule.to_dict())
+
+
+# GET all schedules
+@app.route('/schedules', methods=['GET'])
+def get_schedules():
+    schedules = Schedule.query.all()
+    return jsonify([schedule.to_dict() for schedule in schedules])
+
+# PUT schedule by id
+@app.route('/schedules/<int:schedule_id>', methods=['PUT'])
+def update_schedule(schedule_id):
+    data = request.get_json(force=True)
+    schedule = Schedule.query.get(schedule_id)
+
+    if not schedule:
+        return jsonify({'error': 'Schedule not found'}), 404
+
+    # Update the schedule with the new data
+    schedule.name = data.get('name', schedule.name)
+    schedule.description = data.get('description', schedule.description)
+    
+    # Convert the start_time string to a datetime.time object
+    start_time_str = data.get('start_time', None)
+    if start_time_str:
+        schedule.start_time = datetime.strptime(start_time_str, "%H:%M:%S").time()
+    
+    schedule.frequency = data.get('frequency', schedule.frequency)
+
+    watering_tasks_data = data.get('watering_tasks', [])
+    if watering_tasks_data:
+        for wt_data in watering_tasks_data:
+            watering_task = WateringTask.query.get(wt_data['id'])
+            if watering_task:
+                watering_task.duration = wt_data.get('duration', watering_task.duration)
+                watering_task.sprinkler_id = wt_data.get('sprinkler_id', watering_task.sprinkler_id)
+            else:
+                new_watering_task = WateringTask(duration=wt_data['duration'], sprinkler_id=wt_data['sprinkler_id'], schedule_id=schedule.id)
+                db.session.add(new_watering_task)
+    db.session.commit()
+
+    return jsonify(schedule.to_dict())
+
+
+@app.route('/schedules/<int:schedule_id>', methods=['DELETE'])
+def delete_schedule(schedule_id):
+    Schedule.query.filter_by(id=schedule_id).delete()
+    db.session.commit()
+    return jsonify({'message': f'Schedule {schedule_id} deleted successfully'}), 200
+
+# GET watering tasks by schedule id
+@app.route('/schedules/<int:schedule_id>/watering_tasks', methods=['GET'])
+def get_watering_tasks(schedule_id):
+    watering_tasks = WateringTask.query.filter_by(schedule_id=schedule_id).all()
+    return jsonify([watering_task.to_dict() for watering_task in watering_tasks])
+
+# GET all watering tasks
+@app.route('/watering_tasks', methods=['GET'])
+def get_all_watering_tasks():
+    watering_tasks = WateringTask.query.all()
+    return jsonify([watering_task.to_dict() for watering_task in watering_tasks])
+
+
+# GET all sprinklers
+@app.route('/sprinklers', methods=['GET'])
+def get_sprinklers():
+    sprinklers = Sprinkler.query.all()
+    return jsonify([sprinkler.to_dict() for sprinkler in sprinklers])
+
+
+
+#===================================================================================================
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/toggle', methods=['POST'])
+def toggle_relay():
+    data = request.get_json(force=True)
+    channel = int(data.get('channel'))
+    state = data.get('state') == 'on'
+    relay.control_channel(channel, state)
+    return jsonify({'status': 'success'})
+
+@app.route('/get-events', methods=['GET'])
+def get_events():
+    # Fetch your events from your database or wherever they're stored
+    events = [
+        {
+            'title': 'All Day Event',
+            'start': '2023-07-01'
+        }
+    ]
+    return jsonify({'events': events})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+
---
+++ relay.py
@@ -0,0 +1,56 @@
+import serial
+from time import sleep
+
+port = 'COM4'  # change this to YOUR usb port
+ser = serial.Serial(port, 9600, timeout=5)
+
+delay = 0.1
+tap_delay = 2
+ready_delay = 3
+off_delay = .5
+relay_count = 17
+
+# setup the array of commands to send to the relay
+# Item zero is the status command and the status return
+# Items 1-16 are the commands to turn on each channel
+# Items 17-32 are the commands to turn off each channel
+commandTable = [
+    [b':FE0100200000FF\r\n', b':FE0100000010F1\r\n'],  # status & status return
+    [b':FE0500000000FD\r\n', b':FE050000FF00FE\r\n'],  # channel-1
+    [b':FE0500010000FC\r\n', b':FE050001FF00FD\r\n'],  # channel-2
+    [b':FE0500020000FB\r\n', b':FE050002FF00FC\r\n'],  # channel-3
+    [b':FE0500030000FA\r\n', b':FE050003FF00FB\r\n'],  # channel-4
+    [b':FE0500040000F9\r\n', b':FE050004FF00FA\r\n'],  # channel-5
+    [b':FE0500050000F8\r\n', b':FE050005FF00F9\r\n'],  # channel-6
+    [b':FE0500060000F7\r\n', b':FE050006FF00F8\r\n'],  # channel-7
+    [b':FE0500070000F6\r\n', b':FE050007FF00F7\r\n'],  # channel-8
+    [b':FE0500080000F5\r\n', b':FE050008FF00F6\r\n'],  # channel-9
+    [b':FE0500090000F4\r\n', b':FE050009FF00F5\r\n'],  # channel-10
+    [b':FE05000A0000F3\r\n', b':FE05000AFF00F4\r\n'],  # channel-11
+    [b':FE05000B0000F2\r\n', b':FE05000BFF00F3\r\n'],  # channel-12
+    [b':FE05000C0000F1\r\n', b':FE05000CFF00F2\r\n'],  # channel-13
+    [b':FE05000D0000F0\r\n', b':FE05000DFF00F1\r\n'],  # channel-14
+    [b':FE05000E0000FF\r\n', b':FE05000EFF00F0\r\n'],  # channel-15
+    [b':FE05000F0000FE\r\n', b':FE05000FFF00FF\r\n'],  # channel-16
+    [b':FE0F00000010020000E1\r\n', b':FE0F0000001002FFFFE3\r\n']  # all channels
+]
+
+relay_states = [False] * relay_count
+
+def control_channel(channel, on):
+    # First check if the state has changed
+    if relay_states[channel - 1] != on:
+        # If it has, update the state in the array and change the actual relay
+        relay_states[channel - 1] = on
+        if on:
+            print_and_write(commandTable[channel][1])
+            sleep(delay)
+        else:
+            print_and_write(commandTable[channel][0])
+            sleep(off_delay)
+
+def print_and_write(command):
+    print("Command to send: ", command)
+    ser.write(command)
+
+
