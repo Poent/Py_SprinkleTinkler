3d5f4152a0bad0d4e80ee4d3b0012503ba987366
Poent <Poent@users.noreply.github.com>
2023-07-16 04:24:40 -0600

    working on darkmode html, css, and bootstrap crap.... remind me to never become a web developer.

---
+++ static/js/common.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // This code will run after the page has loaded
+
+    // Dark mode toggling
+    $('#dark-mode-toggle').click(function() {
+        $('body').toggleClass('bg-dark');
+        $('body').toggleClass('text-light');
+        $('table').toggleClass('table-light table-dark');
+        $('.btn').toggleClass('btn-light');
+        
+        const darkMode = $('body').hasClass('bg-dark');
+        localStorage.setItem('dark-mode', darkMode);
+    });
+
+    if(localStorage.getItem('dark-mode') === 'true') {
+        $('body').addClass('bg-dark');
+        $('body').addClass('text-light');
+        $('table').addClass('table-info');
+        $('.btn').addClass('btn-light');
+    }
+
+    // Other common JavaScript code...
+});
